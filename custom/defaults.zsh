0bcd58e950d0e350d3db7179f910675bb14145fb
Jack Scott <jack@nine78.com>
2016-10-04 19:42:32 -0400

    comment out java home

--- custom/defaults.zsh
+++ custom/defaults.zsh
@@ -1,6 +1,6 @@
 export EDITOR=nano
 export LEIN_SNAPSHOTS_IN_RELEASE="override"
-export JAVA_HOME="$(/usr/libexec/java_home)"
+#export JAVA_HOME="$(/usr/libexec/java_home)"
 export LANG=en_US
 export DICTIONARY=en_US
 export PYENV_ROOT=/usr/local/var/pyenv
